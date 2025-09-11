efe55cb870940e5ee1fdca87b615e34d9d0a849e
Kalidoss Anandh <kiyotaka444@duck.com>
2025-09-11 16:01:51 +0530

    fixed booking list

--- public/user/booking.js
+++ public/user/booking.js
@@ -1,22 +1,37 @@
 const listAvailable = async () => {
-  const movieId = window.location.href.split('?');
-  const details = await (await fetch(`/api/showDetails/${movieId[1]}`, { method: 'GET' })).json();
-  const movie = await (await fetch(`/api/stats/movieName/${movieId[1]}`, { method: 'GET' })).json();
+  const movieId = window.location.href.split('?')[1];
+  const details = await (await fetch(`/api/showDetails/${movieId}`, { method: 'GET' })).json();
+  const movie = await (await fetch(`/api/stats/movieName/${movieId}`, { method: 'GET' })).json();
   const item_div = document.querySelector('.container');
+  const theatres = new Map();
+  for (let detail of details) {
+    theatres.set(detail.theatre_id, []);
+  }
+  for (let detail of details) {
+    theatres.get(detail.theatre_id).push(`${detail.show_time} ₹${detail.price}`);
+  }
   item_div.innerHTML = `<h1>${movie[0].title}<h1>`;
-  details.forEach((element, index) => {
-    const item = document.createElement('div');
-    const time = element.show_time.split('T');
-    item.innerHTML = `
-      <div class="theatre row">
-        <h2>${element.name}</h3>
-        <div class="show col">
-          <h3>${time[1].slice(0, 5)}</h3>
-          <h5>${element.price}</h5>
-        </div>
-      </div>
-    `;
-    item_div.appendChild(item);
+  theatres.forEach((element, key) => {
+    const theatreRow = document.createElement('div');
+    const theatreName = document.createElement('h2');
+    const showsRow = document.createElement('div');
+    showsRow.classList.add('shows');
+    showsRow.classList.add('row');
+    for (let show of element) {
+      const showCol = document.createElement('div');
+      showCol.classList.add('show');
+      showCol.classList.add('col');
+      showCol.innerHTML = `<h3>${new Date(show.split(' ')[0])
+        .toLocaleTimeString()
+        .slice(0, -3)}</h3><h5>${show.split(' ')[1]}</h5>`;
+      showsRow.appendChild(showCol);
+    }
+    theatreName.innerHTML = details.find((detail) => detail.theatre_id === key).name;
+    theatreRow.classList.add('theatre');
+    theatreRow.classList.add('row');
+    theatreRow.appendChild(theatreName);
+    theatreRow.appendChild(showsRow);
+    item_div.appendChild(theatreRow);
   });
 };
 
